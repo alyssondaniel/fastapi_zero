02dfc027e463bba6ba97634e8659d57e1ffc8d95
Alysson Daniel Nascimento <alyssondaniel@gmail.com>
2024-06-18 08:32:26 -0300

    Improve tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ from fast_zero.app import app
 
 
 def test_get_root_path_and_return_ok_and_hello_world_json():
-    client = TestClient(app)
-    reponse = client.get('/')
+    textClient = TestClient(app)
+    reponse = textClient.get('/')
     assert reponse.status_code == HTTPStatus.OK
     assert reponse.json() == {'message': 'Olá Mundo!'}
