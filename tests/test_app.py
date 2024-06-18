b399e91574f3a9a09d702fd63f16e3d85d49f9ab
Alysson Daniel Nascimento <alyssondaniel@gmail.com>
2024-06-18 09:01:54 -0300

    Improve endpoints and add database

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
+from fast_zero.schemas import UserPublic
 
 
 def test_get_root_path_and_return_ok_and_hello_world_json():
@@ -10,3 +11,55 @@ def test_get_root_path_and_return_ok_and_hello_world_json():
     reponse = textClient.get('/')
     assert reponse.status_code == HTTPStatus.OK
     assert reponse.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'users': []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client, user):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
